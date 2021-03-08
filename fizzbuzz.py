4ec1bf8223c2142bb0c28467fee483e4a4a69572
admin <admin@visualmachine.localdomain>
2021-03-08 19:57:03 +0300

    now fizzbuzz works with string, which can help remove i % 15 == 0 scenario

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,9 @@
-b="Buzz"
-f="Fizz"
-
 for i in range(101):    # берем 101, потому что range(n) создает массив от 0 до n-1
-    if i % 15 == 0:
-        print(f+b)
-    elif i % 5 == 0:
-        print(b)
-    elif i % 3 == 0:
-        print(f)
-    else:
-        print(i)
+    str = ""
+    if i % 3 == 0:
+        str = "Fizz"
+    if i % 5 == 0:
+        str += "Buzz"
+    if str == "":
+        str = i
+    print(str)
