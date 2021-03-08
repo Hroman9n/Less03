939350ae8e757686f4a87757a7c689845fcc4c71
admin <admin@visualmachine.localdomain>
2021-03-08 19:10:43 +0300

    added numbers frequency script

---
+++ numbers_frequency.py
@@ -0,0 +1,23 @@
+
+numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]    # массив с 10 позициями для подсчета частоты
+
+from_to = list(input("Введите два числа, в диапазоне которых необходимо посчитать частоту: ").split())
+
+if len(from_to) < 2:
+    print("\nВведено меньше 2 чисел\n")
+elif len(from_to) > 2:
+    print("\nВведено больше 2 чисел\n")
+else:
+    from_to = list(map(int, from_to))
+    start = min(from_to)
+    end = max(from_to)
+    
+    for i in range(start,(end+1)):
+        for j in str(i):
+            index = int(j)
+            numbers[index] = numbers[index]+1
+    
+    print(f"\nВ диапазоне от {start} до {end} частота чисел следующая:\n")
+
+    for i in range(len(numbers)):
+        print(f'число {i} повтораяется {numbers[i]} раз\n')
