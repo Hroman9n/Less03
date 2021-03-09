7e8e7a2fe4aefee9955069cde28ca6472c835fb9
Hroman9n <s.vasin99@mail.ru>
2021-03-09 12:31:26 +0300

    & C:/Users/Серёга/AppData/Local/Programs/Python/Python36/python.exe d:/Linux/Python/Less3/xor.py

--- xor.py
+++ xor.py
@@ -1,16 +1,25 @@
-from itertools import cycle
+from itertools import cycle     # необходимо для прокручивания ключа
 
 def do_cipher(string, key):
     return ''.join(chr(ord(s) ^ ord(k)) for (s, k) in zip(string, cycle(key)))
+        #  ^                                здесь мы идем по строке, и ключу
+        # пустая строка,                    прокручивая бесконечно ключ (cycle)
+        # к которой доба-                   объединяя числ. представление  
+        # вляются символы                   каждого символа и получаем новый
+
+### тестовые значения ###
+# key = "01000"
+# key = "fox"
+# key = "midnight"
+# String = "11101"
+# String = "hello, my name is Sergei, nice to meet you"
+# String = "It WaS a GrEaT dAy"
 
 
-key = "01000"
-String = "11101"
+key = input("Key: ")
+String = input("Phrase: ")
 
-# key = input("Key: ")
-# String = input("Phrase: ")
 result = do_cipher(String, key)
 
-# print(str.encode(result))
-print(result)
+print(str.encode(result))
 print(do_cipher(result, key))
